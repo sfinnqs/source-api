6872219f0915bbbc7cdbe9f61c5183fdcdc2b730
sfinnqs <email@sfinnqs.org>
2019-09-05 23:09:29 -0500

    Reworded license header

--- src/main/java/org/sfinnqs/source/OpenSource.java
+++ src/main/java/org/sfinnqs/source/OpenSource.java
@@ -2,8 +2,8 @@
  * Copyright (C) 2019 Finn Voichick
  *
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, version 3.
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
