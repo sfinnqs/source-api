f41946fba424b354723f413c5fcdd16e095b4f0b
sfinnqs <email@sfinnqs.org>
2019-09-11 11:03:24 -0500

    Pseudonym in license notice

--- src/main/java/org/sfinnqs/source/OpenSource.java
+++ src/main/java/org/sfinnqs/source/OpenSource.java
@@ -1,5 +1,5 @@
 /* The Source API - An API for the Source plugin
- * Copyright (C) 2019 Finn Voichick
+ * Copyright (C) 2019 sfinnqs
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of version 3 of the GNU General Public License as published by the
